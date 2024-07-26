a53cb7614b94e10053b0d0148b25fa117c451485
Jimmy Mayta <jimmymayta29@gmail.com>
2024-07-25 22:16:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// console.log ('hola mundo');
+
+// // VARIABLES
+
+// // ES5 : 
+// // VAR (DABA PROBLEMAS DE SCOPE)
+
+
+
+// // ES6
+// // LET : no permite redeclarar la variable, solo redefinir el valor. permite crear la variable y no definirla
+
+// let name = 'Joseph'; 
+
+// // no permite redeclarar el valor
+// //  let name = 'josue';
+// name = 'josue';
+
+// console.log(name);
+
+
+// // CONST : siempre debe inicializar un valor 
+// // no se puede redefinir el valor
+
+// const year = 2024;
+
+// console.log(year)
+
+
+
+
+// let fullName = 'Joseph Mayta';
+
+// // NOMBRAMIENTO DE VARIABLES
+// // 1. Nombrar en ingles 
+// // 2. Nombrar variables con standartd camelcase
+
+// // Camelcase  = fullName (la primera palabra en minuscula y la segundo en mayuscula)
+// // underscore = full_name 
+// // pascalcase = FullName
+
+// let personanombre = 'diego'; 
+
+
+// STRING 
+// el metodoo string define
+
+// const fullName = 'joseph mayta';
+// const greetings = 'Hola estoy crando mi variable'; 
+const namePerson = 'Joseph'; 
+const lastName = 'Mayta'; 
+
+// CONCATENACION ES5
+console.log('hola' + ' ' + namePerson + ' ' + lastName + ' ' + 'Bienvenido!');
+
+// CONCATENACION ES6
+console.log(`hola ${namePerson} ${lastName} Bienvenido!`);
+
+
+
+
+// console.log(greetings.length);
+// console.log(fullName.toLocaleLowerCase)
+//  METODOS 
+
+
+
+
+// el length devuelve el numero de caracteres (es una propiedad, no es necesario el parentesis)
+// TRIM : (metodo) necesita parentesis
+
